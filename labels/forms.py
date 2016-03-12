fc73f090d6cad1c7261aa11494959f56474f113b
Yábir Garcia <zanklord@gmail.com>
2016-03-12 19:05:32 +0100

    Foormtools was separated from django and now needs to be istalled. Corrected the import

--- labels/forms.py
+++ labels/forms.py
@@ -1,11 +1,11 @@
 from django.forms import ModelForm
-from django.contrib.formtools.preview import FormPreview
 from labels.models import Tag
+from formtools.preview import FormPreview
 from django.http import HttpResponseRedirect, HttpResponse
 
 
 class TagForm(ModelForm):
-        
+
     class Meta:
         model = Tag
-        
+        fields = '__all__'
