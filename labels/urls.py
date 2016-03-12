5fae7bf27e97e3d54660667994a2b82beb0be249
Yábir Garcia <zanklord@gmail.com>
2016-03-12 20:32:19 +0100

    Updated for django1.10

--- labels/urls.py
+++ labels/urls.py
@@ -9,15 +9,12 @@ from labels import views as views
 
 
 
-urlpatterns = patterns('',
-
-    #(r'new/$', create_object, {'model': Tag} ),
-    url(r'new/$', 'labels.views.new' ),
+urlpatterns = [
+    url(r'new/$', views.new),
 
     url(r'edit/(?P<pk>\d+)/?$', views.ListUpdate.as_view() ),
     url(r'delete/(?P<pk>\d+)/?$', views.ObjDelete.as_view() ),
 
-    url(r'print/(?P<tag_id>\d+)/(?P<type>\w+)/$', 'labels.views.print_pdf'),
-    url(r'$', views.list.as_view(), name= "home"),
-
-)
+    url(r'print/(?P<tag_id>\d+)/(?P<type>\w+)/$', views.print_pdf),
+    url(r'^$', views.list.as_view(), name= "home")
+]
