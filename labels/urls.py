8592acb99c806a6ad1279cb82b23b991ae683f86
Yábir Garcia <zanklord@gmail.com>
2016-03-12 20:03:58 +0100

    url is not in .default, removed logic from views

--- labels/urls.py
+++ labels/urls.py
@@ -1,27 +1,26 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from labels.forms import TagForm
 from labels.models import Tag
 
 from django import forms
 
-from django.views.generic.create_update import delete_object, update_object
+#from django.views.generic.create_update import delete_object, update_object
 
-from django.views.generic.list_detail import object_list
+#from django.views.generic.list_detail import object_list
+
+from labels import views as views
 
 
 
 urlpatterns = patterns('',
-    
+
     #(r'new/$', create_object, {'model': Tag} ),
     url(r'new/$', 'labels.views.new' ),
-    
-    (r'edit/(?P<object_id>\d+)/?$', update_object, {'form_class': TagForm} ),
-    (r'delete/(?P<object_id>\d+)/?$', delete_object, {'model': Tag, 'post_delete_redirect':'/labels/list/'} ),
-    
-    (r'list/$', object_list, {'queryset': Tag.objects.all()}),
+
+    url(r'edit/(?P<pk>\d+)/?$', views.ListUpdate.as_view() ),
+    url(r'delete/(?P<pk>\d+)/?$', views.ObjDelete.as_view() ),
+
     url(r'print/(?P<tag_id>\d+)/(?P<type>\w+)/$', 'labels.views.print_pdf'),
-    (r'$', object_list, {'queryset': Tag.objects.all()}),
-       
+    url(r'$', views.list.as_view(), name= "home"),
+
 )
-
-
